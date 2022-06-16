4c4d07527f16efa95505f272641156dc6b21ba43
Parzalini <106882104+Parzalini@users.noreply.github.com>
2022-06-16 21:07:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-//const hamburger = document.querySelector(".hamburger-lines");
-//const menuLinks = document.querySelector(".menu-links");
-
-//hamburger.addEventListener("click", function () {
-//menuLinks.classList.add("open");
-//});
-
 const swiper = new Swiper(".swiper", {
   autoplay: {
     delay: 8000,
